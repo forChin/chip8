542c856dc4e46b0ce8ca2ef6bfbc0264499a90c6
forChin <chingiz_seitov@mail.ru>
2021-02-19 23:31:52 +0600

    Remove unnecessary code

--- util.go
+++ util.go
@@ -6,8 +6,3 @@ func randByte() byte {
 	max := 256                  // 255 + 1 because rand.Intn return int >= 0, < n
 	return byte(rand.Intn(max)) // will return num in [0, 256)
 }
-
-func pixelSetAt(screen [32]uint64, x int, y int) bool {
-	columnFilter := uint64(1) << (63 - uint(x))
-	return screen[y]&columnFilter == columnFilter
-}
