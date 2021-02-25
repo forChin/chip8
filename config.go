7ffb8e0f47a2f0e661544a5c98981594d5b06afb
forChin <chingiz409@gmail.com>
2021-02-25 10:57:33 +0600

    Change opcodePerSecond type and some default values

--- config.go
+++ config.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/caarlos0/env/v6"
 )
 
 type config struct {
-	WindowW          int32         `env:"WINDOW_WIDTH" envDefault:"320"`
-	WindowH          int32         `env:"WINDOW_HEIGHT" envDefault:"540"`
-	OpcodesPerSecond time.Duration `env:"OPCODES_PER_SECOND" envDefault:"300"`
+	WindowW          int32 `env:"WINDOW_WIDTH" envDefault:"320"`
+	WindowH          int32 `env:"WINDOW_HEIGHT" envDefault:"640"`
+	OpcodesPerSecond int   `env:"OPCODES_PER_SECOND" envDefault:"300"`
 
 	ROMPath string `env:"GAME_ROM_PATH"`
 }
 
 func newConfig() (*config, error) {
-	var cfg config
+	cfg := config{}
 	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
